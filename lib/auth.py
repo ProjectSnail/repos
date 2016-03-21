977fd4a03c83e873320f93fed7d7237379ba855f
Edgar Albalate Ibañez <dev.gardo@gmail.com>
2016-03-21 17:00:28 +0100

    Started lib file auth.py to manage authorization

---
+++ lib/auth.py
@@ -0,0 +1,40 @@
+import os, pickle, getpass
+
+USER_LOCAL_PATH = os.path.join(os.environ["HOME"], ".repos")
+USER_AUTH_PATH = os.path.join(USER_LOCAL_PATH, 'auth')
+
+def checkFolder():
+    if not os.path.exists(USER_LOCAL_PATH):
+        os.makedirs(USER_LOCAL_PATH)
+        return False
+    return True
+
+def getAuth():
+    if checkFolder():
+        if os.path.isfile(USER_AUTH_PATH):
+            with open(USER_AUTH_PATH, 'rb') as f:
+                pickle.load(f)
+                l = pickle.load(f)
+                usr = f.read(l)
+                l = pickle.load(f)
+                pwd = f.read(l)
+                return (usr, pwd)
+        return None
+    return None
+
+def writeAuth(usr, pwd):
+    checkFolder()
+    with open(USER_AUTH_PATH, 'wb') as f:
+        pickle.dump(0, f)
+        pickle.dump( len(usr), f)
+        f.write(usr.encode('ascii'))
+        pickle.dump( len(pwd), f)
+        f.write(pwd.encode('ascii'))
+
+def askAuth():
+    username = raw_input("Auth required.\n  username: ")
+    password = getpass.getpass("  password: ")
+    return (username, password)
+
+if __name__ == "__main__" :
+    pass
--- repos.py
+++ repos.py
@@ -7,12 +7,12 @@ repos                               list all existing repos
 repos list [username]               list all repos of given user, default auth
 repos create    :name               create a new repo, default public
 repos edit      :name               edits repo values
-repos remove    :name               removes a repo
+repos delete    :name               removes a repo
 """
 
 """  Options:
- --Org, -o                       :   given username is for organization
-
+ --Org, -o :orgname                 given name is for organization
+ --Private, -p
 """
 
 import requests, json, colored, time
